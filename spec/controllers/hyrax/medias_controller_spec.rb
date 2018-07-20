4bae2836be47d14ffb834e1bc5b49d28c0c26c99
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-07-20 16:16:19 -0400

    delete residual medias_controller_spec

--- spec/controllers/hyrax/medias_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Media`
-require 'rails_helper'
-
-RSpec.describe Hyrax::MediasController do
-  it "has tests" do
-    skip "Add your tests here"
-  end
-end
