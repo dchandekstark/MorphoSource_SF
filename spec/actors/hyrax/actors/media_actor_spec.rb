22e3d9fac879af85d81c509fc0e3166deedaea67
JocelynTriplett <jocelyn.triplett@gmail.com>
2018-09-28 14:28:52 -0400

    remove commented-out media actor spec

--- spec/actors/hyrax/actors/media_actor_spec.rb
+++ spec/actors/hyrax/actors/media_actor_spec.rb
@@ -3,57 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::Actors::MediaActor do
-
-  # No longer using the Media Actor to concatenate the scale bar. Keeping code here for future reference when writing tests. 
-
-  # subject(:middleware) do
-  #   stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
-  #     middleware.use described_class
-  #   end
-  #   stack.build(terminator)
-  # end
-  #
-  # let(:user) { FactoryBot.build(:user) }
-  # let(:ability) { Ability.new(user) }
-  # let(:media) { FactoryBot.create(:media)}
-  # let(:env) { Hyrax::Actors::Environment.new(media, ability, attributes) }
-  #
-  # let(:terminator) { Hyrax::Actors::Terminator.new }
-  #
-  # describe '#create' do
-  #
-  #   before do
-  #     middleware.create(env)
-  #   end
-  #
-  #   describe 'scale bar' do
-  #
-  #     let(:attributes) { { title: ["example media title"],
-  #     scale_bar_target_type: ["example type"], scale_bar_distance: ["example distance"],
-  #     scale_bar_units: ["example units"], scale_bar: [""]} }
-  #
-  #     it 'concatenates type, distance, and units' do
-  #       expect(env.attributes[:scale_bar]).to contain_exactly("Type: example type, Distance: example distance, Units: example units")
-  #     end
-  #   end
-  # end
-  #
-  # describe '#update' do
-  #
-  #   before do
-  #     middleware.update(env)
-  #   end
-  #
-  #   describe 'scale bar' do
-  #
-  #     let(:attributes) { { title: ["example new media title"],
-  #     scale_bar_target_type: ["example new type"], scale_bar_distance: ["example new distance"],
-  #     scale_bar_units: ["example new units"], scale_bar: [""]} }
-  #
-  #     it 'concatenates updated type, distance, and units' do
-  #       expect(env.attributes[:scale_bar]).to contain_exactly("Type: example new type, Distance: example new distance, Units: example new units")
-  #     end
-  #   end
-  # end
-
+  it "has tests" do
+    skip "Add your tests here"
+  end
 end
