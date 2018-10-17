62b82bdada2cb18608fe7799700584f6112aedba
Julie Winchester <julia.m.winchester@gmail.com>
2018-10-17 13:10:07 -0400

    Delete extraneous countries_service.rb file

--- app/services/Morphosource/countries_service.rb
+++
@@ -1,9 +0,0 @@
-# app/services/morphosource/countries_service.rb
-module Morphosource
-  # Provide select options for the country field in Institution work
-  class CountriesService < Hyrax::QaSelectService
-    def initialize(_authority_name = nil)
-      super('countries')
-    end
-  end
-end
