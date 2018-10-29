f1f7669b27db6bad2060b2dcad6072a04d8280f8
Simon Choy <simon.choy@duke.edu>
2018-10-29 14:57:45 -0400

    overriding FileSetPresenter in config/initializers/morphosource.rb instead

--- config/initializers/morphosource.rb
+++ config/initializers/morphosource.rb
@@ -1,8 +1,11 @@
 require 'hydra/works/characterization/schema/image_ext_schema.rb'
 require 'hydra/works/characterization/schema/dicom_schema.rb'
+require 'hyrax/search_state' # this is needed for overriding FileSetPresenter with MsFileSetPresenter
 
 ActiveFedora::WithMetadata::DefaultMetadataClassFactory.file_metadata_schemas +=
-    [ 
-        Hydra::Works::Characterization::ImageExtSchema,
-        Hydra::Works::Characterization::DicomSchema
-    ]
+[ 
+  Hydra::Works::Characterization::ImageExtSchema,
+  Hydra::Works::Characterization::DicomSchema
+]
+
+Hyrax::FileSetsController.show_presenter = Hyrax::MsFileSetPresenter
