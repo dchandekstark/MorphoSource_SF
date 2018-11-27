e0a1f6480662675c68644b3db478cd553a075ea6
Jim Coble <jim.coble@duke.edu>
2018-11-27 14:43:29 -0500

    Add 'Start Over' button to all submission pages except the first; addresses MR-329. (#86)

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -10,24 +10,29 @@ class SubmissionsController < ApplicationController
 
   def create
     # TODO: In serious need of refactoring
-    session[:submission_params].deep_merge!(submission_params) if params[:submission]
-    @submission = Submission.new(session[:submission_params])
-    @submission.current_step = Submission.step(session[:submission_step])
-    if params[:previous_button]
-      @submission.step_back
-    elsif @submission.valid?
-      if @submission.last_step?
-        @submission.save if @submission.all_valid?
-        redirect_to [ main_app, :new, :hyrax, :parent, 'media', parent_id: @submission.imaging_event_id ]
-      else
-        @submission.step_forward
-      end
-    end
-    session[:submission_step] = @submission.current_step.name
-    if @submission.new_record?
-      render 'new'
-    else
+    if params[:start_over]
       session[:submission_step] = session[:submission_params] = nil
+      redirect_to new_submission_path
+    else
+      session[:submission_params].deep_merge!(submission_params) if params[:submission]
+      @submission = Submission.new(session[:submission_params])
+      @submission.current_step = Submission.step(session[:submission_step])
+      if params[:previous_button]
+        @submission.step_back
+      elsif @submission.valid?
+        if @submission.last_step?
+          @submission.save if @submission.all_valid?
+          redirect_to [ main_app, :new, :hyrax, :parent, 'media', parent_id: @submission.imaging_event_id ]
+        else
+          @submission.step_forward
+        end
+      end
+      session[:submission_step] = @submission.current_step.name
+      if @submission.new_record?
+        render 'new'
+      else
+        session[:submission_step] = session[:submission_params] = nil
+      end
     end
   end
 
