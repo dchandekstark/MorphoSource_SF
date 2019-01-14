abea510bdcbcd33aac3875e447ce8149a55c9f39
JocelynTriplett <jocelyn.triplett@gmail.com>
2019-01-14 10:13:10 -0500

    display file visibility on dashboard works list

--- app/models/media.rb
+++ app/models/media.rb
@@ -15,4 +15,31 @@ class Media < Morphosource::Works::Base
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
+
+  # array of all visibilities that apply to the file sets of a Media work
+  # used to populate File Visibility column in dashboard works list
+  def file_set_visibilities
+
+    all_visibilities = [
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC,
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED,
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_EMBARGO,
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_LEASE,
+      Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE ]
+
+    file_visibilities = []
+
+    file_set_ids.each do |id|
+      file = FileSet.find(id)
+      if file.embargo_id.present?
+        file_visibilities << "embargo"
+      elsif file.lease_id.present?
+        file_visibilities << "lease"
+      else
+        file_visibilities << file.visibility
+      end
+    end
+    # order unique visibilities in the order that they appear on the work form.
+    all_visibilities & file_visibilities
+  end
 end
