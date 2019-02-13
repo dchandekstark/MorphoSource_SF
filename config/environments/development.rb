0af5d2ad55d6cc79eaa505379c527f1f97c2b421
Ryan Baumann <ryan.baumann@gmail.com>
2019-02-13 09:18:58 -0500

    Cache classes in development mode.
    This should improve development mode slowness, and also seems to
    fix "stack level too deep" errors.
    
    The problem seems to have been that without class caching, on every
    request (and particularly any one that invokes a URL helper `*_path`
    method), a huge number of dynamically generated URL helper methods
    for routes have to be generated, some recursively.
    
    Setting `config.reload_classes_only_on_change = true` should get
    us the benefits of setting `config.cache_classes = true`, while
    also allowing for edits to be reflected live without server
    restarts.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,5 +63,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.reload_classes_only_on_change = false
+  config.reload_classes_only_on_change = true
 end
