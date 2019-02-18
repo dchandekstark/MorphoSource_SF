718dfff88df222ff2b0f7a7a02f5788eb4f5a413
Ryan Baumann <ryan.baumann@gmail.com>
2019-02-18 10:16:18 -0500

    Add spec for routing to zip actions

---
+++ spec/routing/zip_routing_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'zip routing', type: :routing do
+
+  it 'has a new route' do
+    route = { controller: 'hyrax/file_sets', action: 'zip' }
+    expect(:get => '/concern/file_sets/zip').to route_to(route)
+  end
+
+end
