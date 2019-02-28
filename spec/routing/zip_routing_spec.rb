ecd273579f658ca14a920d4ce6e2b99d8bedd75d
Ryan Baumann <ryan.baumann@gmail.com>
2019-02-28 15:21:52 -0500

    Spec MediaController#zip route

--- spec/routing/zip_routing_spec.rb
+++ spec/routing/zip_routing_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'zip routing', type: :routing do
 
   it 'has a new route' do
+    route = { controller: 'hyrax/media', action: 'zip' }
+    expect(:get => '/concern/media/zip').to route_to(route)
+
     route = { controller: 'hyrax/file_sets', action: 'zip' }
     expect(:get => '/concern/file_sets/zip').to route_to(route)
   end
