8f94bb1eed2f5fc9fa7712400f8db73d50f839c8
Simon Choy <simon.choy@duke.edu>
2019-03-07 15:29:30 -0500

    add general details content

--- app/presenters/hyrax/biological_specimen_presenter.rb
+++ app/presenters/hyrax/biological_specimen_presenter.rb
@@ -7,5 +7,21 @@ module Hyrax
     delegate :bibliographic_citation, :catalog_number, :collection_code, :numeric_time, :original_location,
              :periodic_time, :vouchered, :idigbio_recordset_id, :idigbio_uuid, :is_type_specimen, :occurrence_id, :sex,
              :geographic_coordinates,  to: :solr_document
+  
+
+
+
+   	def parent_title(type)
+    	title = ''
+    	grouped_presenters(filtered_by: type).each_pair do |_, items| 
+        items.each do |item|
+        	title = item.title.first 
+          # link_to item.title.first, url_for_document(item) 
+        end
+			end
+			title 
+    end
+
+
   end
 end
