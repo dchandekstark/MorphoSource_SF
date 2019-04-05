607518ed57375cd1e890ae82bda5519351fe4923
Ryan Baumann <ryan.baumann@gmail.com>
2019-04-05 15:16:48 -0400

    Change CT/MRI image series format wording to include .zip

--- spec/features/media_upload_formats_spec.rb
+++ spec/features/media_upload_formats_spec.rb
@@ -54,7 +54,7 @@ RSpec.feature 'update Media file formats', js: true do
       expect(page).to have_content (Morphosource.video_formats.join(', '))
 
       click_link "Descriptions"
-      select('CT/MRI image series (multiple files of type *.tiff, *.png, *.dcm, etc.)', from: 'Media Type')
+      select('CT/MRI image series (multiple files of type *.tiff, *.png, *.dcm, *.zip, etc.)', from: 'Media Type')
       click_link "Files"
       expect(page).to have_content (Morphosource.ct_formats.join(', '))
 
