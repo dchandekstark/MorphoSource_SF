53e656e48a52926be218b7fb2d11aa2754df2688
Simon Choy <simon.choy@duke.edu>
2019-04-25 20:07:47 -0400

    initial changes for image acq details

--- app/presenters/hyrax/media_presenter.rb
+++ app/presenters/hyrax/media_presenter.rb
@@ -6,9 +6,9 @@ module Hyrax
 
     delegate :agreement_uri, :cite_as, :funding, :map_type, :media_type, :modality, :orientation, :part, :rights_holder, :scale_bar, :series_type, :side, :unit, :x_spacing, :y_spacing, :z_spacing, :slice_thickness, :identifier, :related_url, to: :solr_document
 
-    attr_accessor :physical_object_type, :idigbio_uuid, :vouchered, :physical_object_title, :physical_object_link, :physical_object_id, :device_title, :device_facility, :device_link, :device, :parent_media_id_list, :child_media_id_list,
-      :sibling_media_id_list, :parent_media_count, :direct_parent_title_list, :processing_event_count, :data_managed_by,
-      :download_permission, :ark, :doi
+    attr_accessor :physical_object_type, :idigbio_uuid, :vouchered, :physical_object_title, :physical_object_link, :physical_object_id, :device_and_facility, :device_facility, :device_link, :device, :parent_media_id_list, :child_media_id_list, :sibling_media_id_list, :parent_media_count, :direct_parent_title_list, 
+      :processing_event_count, :data_managed_by, :download_permission, :ark, :doi, :lens, :other_details, 
+      :creator, :date_created
 
     def universal_viewer?
       representative_id.present? &&
@@ -45,11 +45,23 @@ module Hyrax
         # get device from imaging event
         device = Device.where('member_ids_ssim' => imaging_event.id).first
         if device.present?
-          @device_title = device.title.first
+          @device = device.title.first
           @device_facility = device.facility.first
-          @device = device.title.first + " (" + device.facility.first + ")"
+          @device_and_facility = device.title.first + " (" + device.facility.first + ")"
           @device_link = "/concern/devices/" + device.id
         end
+
+        # get imaging event details
+        @lens = ""
+        @lens << imaging_event.lens_make.first if imaging_event.lens_make.present?
+        @lens << " " + imaging_event.lens_model.first if imaging_event.lens_model.present?
+        @other_details = ""
+        @other_details << imaging_event.focal_length_type.first + " focal length" if imaging_event.focal_length_type.present?
+        @other_details << " / " + imaging_event.light_source.first + " light" if imaging_event.light_source.present?
+        @other_details << " / " + imaging_event.background_removal.first if imaging_event.background_removal.present?
+        @creator = imaging_event.creator
+        @date_created = imaging_event.date_created
+
       end # end if imaging_event present?
       
       # add current media id, then add child media ids.  
