d8a6b5017b949e3e44b616b987e5dc7db9822b27
Simon Choy <simon.choy@duke.edu>
2019-06-21 20:38:13 -0400

    remove checking for empty institution list

--- spec/views/submissions/device_institution.html.erb_spec.rb
+++ spec/views/submissions/device_institution.html.erb_spec.rb
@@ -2,27 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'submissions/device_institution' do
   let(:ability) { double Ability }
+  let(:new_institution_title) { ['XXYYZZ'] }
 
   before do
+    assign(:submission, Submission.new)
 		@institution_form = Hyrax::WorkFormService.build(Institution.new, ability, self)
+	  session[:submission_institution_create_params] = ActionController::Parameters.new(title: new_institution_title)
+	  render
   end
 
   describe 'device_institution_select partial' do
-    let(:partial_content) { 'device_institution_select partial content' }
-    let(:new_institution_title) { 'XXYYZZ'}
-
-	  before do
-		  session[:submission_institution_create_params] = ActionController::Parameters.new(title: new_institution_title)
-	  end
-
     it 'manually adds the new Institution to the select dropdown list' do
-      assign(:submission, Submission.new)
-      stub_template 'submissions/_device_institution_select.html.erb' => partial_content
-      render
-      expect(rendered).to match(/#{partial_content}/)
       expect(rendered).to match(/#{new_institution_title}/)
     end
   end
+
   describe 'device_institution_create partial' do
     let(:partial_content) { 'device_institution_create partial content' }
     it 'renders the partial' do
