f72babbb2c4fbf322bef5ff0b14913f3b5f9874b
Simon Choy <simon.choy@duke.edu>
2019-07-24 23:04:12 -0400

    added form loading indicator

--- app/assets/javascripts/hyrax/ms_helpers.js
+++ app/assets/javascripts/hyrax/ms_helpers.js
@@ -20,15 +20,30 @@ function setupEmbeddedForm(work_name) {
 	var this_path = "/submissions/" + work_name;
 
   $(document).on("click", this_btn, function() {			
-		$(this_div).load(this_path);
 		$(this_div).show();
+		$(this_div).addClass('ui-loading');
+		$.ajax({
+		  url: this_path,
+		  success: function(result) {
+		  	try {
+			    $(this_div).html(result);
+		  	} catch (e) {
+		  		// catching the error " incorrect module build, no module name "
+		  		// todo: resolve the error later
+		  		// console.log(e);
+		  	}
+		  	$(this_div).removeClass('ui-loading');
+		  }
+		});
 	});
 
   $(this_div).on("submit", this_form, function() {
 		$(this_div).hide();
+   	$(this_div).html('');
 		return true;
   });
   $(this_div).on("click", "button.cancel", function() {
 		$(this_div).hide();
+   	$(this_div).html('');
   });
 }
