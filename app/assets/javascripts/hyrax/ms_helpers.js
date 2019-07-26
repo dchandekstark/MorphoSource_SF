1c53d90444c93cc521907ba4b83fcabf5966f2b9
Simon Choy <simon.choy@duke.edu>
2019-07-26 15:27:05 -0400

    handle add work button after form submit

--- app/assets/javascripts/hyrax/ms_helpers.js
+++ app/assets/javascripts/hyrax/ms_helpers.js
@@ -19,7 +19,8 @@ function setupEmbeddedForm(work_name) {
 	var this_form = "form#" + work_name;
 	var this_path = "/submissions/" + work_name;
 
-  $(document).on("click", this_btn, function() {			
+  $(document).on("click", this_btn, function() {		
+  	// load the new work form	
 		$(this_div).show();
 		$(this_div).addClass('ui-loading');
 		$.ajax({
@@ -32,15 +33,38 @@ function setupEmbeddedForm(work_name) {
 		  		// todo: resolve the error later
 		  		// console.log(e);
 		  	}
-		  	$(this_div).removeClass('ui-loading');
+		  	$(this_div).removeClass('ui-loading');		    
 		  }
 		});
 	});
 
   $(this_div).on("submit", this_form, function() {
+		// replace with ajax form post to trigger other actions
+		$.post($(this_form).attr('action'),
+	  $(this_form).serialize(), function(data, status){
+	    // to-do: will need to setup proper response from the server side to get return status, work ID, etc. 
+	    //alert("Data: " + data + "\nStatus: " + status);
+
+	    console.log('work created by now...');
+
+	  	var this_input = $('div[data-behavior="parent-relationships-institutions"]').find('input[name="biological_specimen[find_parent_work]"]');
+			this_input.val('x346d4165');
+
+			var this_element = $('[data-behavior="parent-relationships-institutions"]');
+
+			var new_data = {
+				id: "x346d4165", 
+				text: "Duke 724"
+			};
+	    this_element.data('new-work-created', new_data);
+			var this_add_btn = $(this_div).data("add-button");
+			$(this_add_btn).trigger("click");
+	  });
+	  
+//	  console.log(this.input.select2('data'))	
 		$(this_div).hide();
    	$(this_div).html('');
-		return true;
+		return false;
   });
   $(this_div).on("click", "button.cancel", function() {
 		$(this_div).hide();
