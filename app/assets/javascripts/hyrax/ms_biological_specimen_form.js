7e2e6bfecbfc2500bd14a0dcd2359cff11108161
Simon Choy <simon.choy@duke.edu>
2019-07-29 15:28:46 -0400

    setup JSON response and js handling after work is created

--- app/assets/javascripts/hyrax/ms_biological_specimen_form.js
+++ app/assets/javascripts/hyrax/ms_biological_specimen_form.js
@@ -1,6 +1,13 @@
 $(document).on('turbolinks:load', function() {
   if ($('form[id*="biological_specimen"]').length) { // if BSO form page
 //    setupEmbeddedForm('new_taxonomy');
-    setupEmbeddedForm('new_institution');
+    setupEmbeddedWorkForm('new_institution');
+
+    $(document).on("click", "#btn-add-institution", function() {
+      alert('c');
+      // check if an institution exists already
+      
+      return false;
+    });
   }
 });
